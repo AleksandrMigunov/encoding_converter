7518cb9403c4ad8e6cd6e252c8c024d17086895a
Aleksandr Migunov <82398398+AleksandrMigunov@users.noreply.github.com>
2022-11-03 22:20:46 +0300

    Add files via upload

--- EncConverterC/mainwindow.cpp
+++ EncConverterC/mainwindow.cpp
@@ -1,3 +1,19 @@
+/* Copyright (C) 2022 Aleksandr Migunov
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+  
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>. */
+
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QtCore>
@@ -340,6 +356,23 @@ void MainWindow::on_actionSave_As_triggered()
             QMessageBox::information(this, tr("File was saved"),
                                      tr("File was saved as") + " " + FileName);
         }
+    }
+}
+
+//closing the program
+void MainWindow::closeEvent(QCloseEvent *event) {
+    QMessageBox* messagebox = new QMessageBox (QMessageBox::Question,
+                                               tr("Confirm Dialog"), tr("Really quit?"),
+                                               QMessageBox::Yes | QMessageBox::No, this);
+    messagebox->setButtonText(QMessageBox::Yes, tr("Yes"));
+    messagebox->setButtonText(QMessageBox::No, tr("No"));
+    int n = messagebox->exec();
+    delete messagebox;
+
+    if (n == QMessageBox::Yes) {
+        event->accept();
+    } else {
+        event->ignore();
     }
 }
 
