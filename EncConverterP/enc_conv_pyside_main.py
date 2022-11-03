aabec009b2220a3be11d8f398f549926b9b2d295
Aleksandr Migunov <82398398+AleksandrMigunov@users.noreply.github.com>
2022-11-03 22:21:17 +0300

    Add files via upload

--- EncConverterP/enc_conv_pyside_main.py
+++ EncConverterP/enc_conv_pyside_main.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2022 Aleksandr Migunov
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+  
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # -*- coding: utf-8 -*-
 
 import sys, os
@@ -21,6 +36,17 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.actionAbout_Qt.triggered.connect(self.aboutQt)
         self.NewOpened = ""
         self.FileOpened = ""
+        
+    def closeEvent(self, e):
+        result = QtWidgets.QMessageBox.question(self, "Confirm Dialog",
+                                                "Really quit?",
+                                                QtWidgets.QMessageBox.Yes |
+                                                QtWidgets.QMessageBox.No,
+                                                QtWidgets.QMessageBox.No)
+        if result == QtWidgets.QMessageBox.Yes:
+            e.accept()
+        else:
+            e.ignore()        
 
     def newFile(self):
         self.NewOpened = ""
